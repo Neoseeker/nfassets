e86e76760bc8b649a19b6dcc248770c65edcffad
Howard Ha <bluespire@gmail.com>
2018-10-05 16:15:18 -0700

    change more links to https versions of neo URLs

--- js/thegreatchai/sr3.js
+++ js/thegreatchai/sr3.js
@@ -1,15 +1,11 @@
 // http://www.neoseeker.com/forums/53549/
 
-var images = new Array('http://i.neoseeker.com/mgv/424318-Randome/318/95/sr32_display.gif',
-'http://img803.imageshack.us/img803/3957/header2.png',
-'http://i54.tinypic.com/2utlrtu.png',
-'http://i56.tinypic.com/zu4c61.png',
+var images = new Array('https://i.neoseeker.com/mgv/424318/318/95/sr32_display.gif',
+'https://i.imgur.com/AJlqA59.png',
 'http://i55.tinypic.com/28su1xj.png',
-'http://i56.tinypic.com/2ed6uiu.png',
-'http://i.neoseeker.com/mgv/424318-Randome/318/14/header_2_display.png',
-'http://i237.photobucket.com/albums/ff146/The_DW_Elmo/SR3.png',
-'http://i237.photobucket.com/albums/ff146/The_DW_Elmo/SR3P.png',
-'http://img810.imageshack.us/img810/442/unled1es.jpg');
+'https://i.imgur.com/qxCV5S5.png',
+'https://i.neoseeker.com/mgv/424318/318/14/header_2_display.png',
+);
 var num = Math.floor(Math.random() * images.length);
 
 $('#random').attr("src", images[num]);
