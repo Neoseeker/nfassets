518e55464b83a116a4da589d1dc19c36331dd1b5
Howard Ha <bluespire@gmail.com>
2018-10-05 19:42:57 -0700

    remove broken image from sr3.js

--- js/thegreatchai/sr3.js
+++ js/thegreatchai/sr3.js
@@ -2,7 +2,6 @@
 
 var images = new Array('https://i.neoseeker.com/mgv/424318/318/95/sr32_display.gif',
 'https://i.imgur.com/AJlqA59.png',
-'http://i55.tinypic.com/28su1xj.png',
 'https://i.imgur.com/qxCV5S5.png',
 'https://i.neoseeker.com/mgv/424318/318/14/header_2_display.png',
 );
